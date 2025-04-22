a4aa13939dcb45847201db7cd3003a10ca76a6f7
Jepherson <jepherson.brc46@gmail.com>
2025-04-22 19:00:57 +0000

    Inclusão do uso de Recursividade

--- xadrez.c
+++ xadrez.c
@@ -4,38 +4,66 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void movertorre (int movimento){
+  if (movimento > 0) {
+    movertorre (movimento  -1);
+    printf("Direita\n");
+  }
+}
+
+void moverBispo (int movimento1){
+  if (movimento1 > 0) {
+moverBispo (movimento1 -1);
+
+  for (int i, j = 0 ; i, j < 1; i++, j++)
+  {
+  printf("Cima Direita\n", i, j);
+  }
+  
+  }
+}
+
+void moverRainha (int movimento2){
+  if (movimento2 > 0) {
+    moverRainha (movimento2 -1);
+    printf("Esquerda\n");
+  }
+}
 int main() {
+int i = 1;
 
 // Implementação de movimento da Torre
 
-int i = 1;
+printf("Movimento da Torre.\n");
+movertorre(5);
 
-printf("Movimento da Torre\n");
-while (i <= 5){
-  printf("direita\n", i);
-  i++;
-}
-
+printf("\n");
 //Implementação de movimento do Bispo
-printf("Movimento do Bispo\n");
 
-for (int i = 1; i < 5; i++){
-printf("cima direita\n", i);
+printf("Movimento do Bispo.\n");
+moverBispo(5);
+ 
+printf("\n");
+//Implementação de movimento da Rainha
+
+printf("Movimento da Rainha.\n");
+moverRainha(8);
+printf("\n");
+    // Nível Aventureiro - Movimentação do Cavalo
+
+    printf("Movimentação do Cavalo.\n");
+    int k = 0;
+ while (k <= 1){
+  
+ printf("Baixo\n");
+ k++;
+    
+  }for (int i = 1; i <= 1; i++)
+{   printf("Direita\n");
+  
+  
 }
 
-//Implementação de movimento da Rainha
-int j = 1;
-printf("Movimento da Rainha\n");
-do
-{
-    printf("esquerda\n");
-    j++;
-} while (j <= 8);
-
-   
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
 
